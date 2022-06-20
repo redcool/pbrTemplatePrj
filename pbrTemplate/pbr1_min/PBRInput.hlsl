41e1523a57b92ad97d4575fa752e402c3a5dad39
ken <redcool007@qq.com>
2022-06-20 17:41:39 +0800

    update pbr_min template

---
+++ pbrTemplate/pbr1_min/PBRInput.hlsl
@@ -0,0 +1,19 @@
+#if !defined(PBR_INPUT_HLSL)
+#define PBR_INPUT_HLSL
+sampler2D _MainTex;
+samplerCUBE unity_SpecCube0;
+sampler2D _NormalMap;
+sampler2D _PBRMask;
+sampler2D _CameraOpaqueTexture;
+sampler2D _CameraDepthTexture;
+
+CBUFFER_START(UnityPerMaterial)
+half _Smoothness;
+half _Metallic;
+half _Occlusion;
+
+half _NormalScale;
+half _Depth;
+
+CBUFFER_END
+#endif //PBR_INPUT_HLSL
