4f5356736fc0868b6653f8318c5e306be14fa854
gamebox1 <redcool007@qq.com>
2026-02-05 18:12:53 +0800

    v(0.0.14.51) update : motionVectors

--- CommonShaders/Billboard/BillLib.hlsl
+++ CommonShaders/Billboard/BillLib.hlsl
@@ -119,7 +119,7 @@
         o.fogCoord.xy = CalcFogFactor(worldPos.xyz,o.vertex.z,_HeightFogOn,_DepthFogOn);
         o.vertexNormal = (v.normal);
 
-        CALC_MOTION_POSITIONS_WORLD((v.prevPos.xyz),worldPos,o,o.vertex);
+        CALC_MOTION_POSITIONS(v.prevPos,v.vertex,o,o.vertex);
 
         return o;
     }
